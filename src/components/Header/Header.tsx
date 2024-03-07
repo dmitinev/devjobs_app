8a9b19697afe8b584cb0718601b02d24843efffa
Dmitry Mitinev <83422156+dmitinev@users.noreply.github.com>
2024-03-07 17:33:48 +0100

    Add link to logo in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/desktop/logo.svg?react';
 import { ThemeSwitcher } from '../ThemeSwitcher/ThemeSwitcher';
 import styles from './Header.module.scss';
 
 export const Header = () => (
   <header className={styles.header}>
-    <Logo />
+    <Link className={styles.header__link} to="/devjobs_app/">
+      <Logo />
+    </Link>
     <ThemeSwitcher />
   </header>
 );
