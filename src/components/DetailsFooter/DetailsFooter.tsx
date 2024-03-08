9799468dd1a42f2085b5f48be85dd5ed6ec69132
Dmitry Mitinev <83422156+dmitinev@users.noreply.github.com>
2024-03-08 19:43:36 +0100

    Update DetailsFooter styles and add dynamic content

--- src/components/DetailsFooter/DetailsFooter.tsx
+++ src/components/DetailsFooter/DetailsFooter.tsx
@@ -1,14 +1,26 @@
+import { useContext } from 'react';
 import { FooterLink } from '../../components/FooterLink';
+import { ApiDataContext } from '../../context/apiDataContext/ApiDataContext';
 import styles from './DetailsFooter.module.scss';
 
 interface DetailsFooterProps {
   pageId: number;
 }
 
-export const DetailsFooter = ({ pageId }: DetailsFooterProps) => (
-  <footer className={styles.detailsFooter}>
-    <div className={styles.footerContainer}>
-      <FooterLink pageId={pageId} />
-    </div>
-  </footer>
-);
+export const DetailsFooter = ({ pageId }: DetailsFooterProps) => {
+  const { originalApiData } = useContext(ApiDataContext);
+  const vacancy = originalApiData.find((vacancy) => vacancy.id === pageId);
+  return (
+    <footer className={styles.detailsFooter}>
+      <div className={styles.footerContainer}>
+        <h3 className={styles.detailsFooter__position}>{vacancy?.position}</h3>
+        <span className={styles.detailsFooter__company}>
+          {vacancy?.company}
+        </span>
+        <div className={styles.detailsFooter__link}>
+          <FooterLink pageId={pageId} />
+        </div>
+      </div>
+    </footer>
+  );
+};
